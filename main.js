f81696682262767b140c08f7b83c491d6aaca005
Иван Бахарев <ragna.blood82@gmail.com>
2024-02-16 12:51:49 +0300

    Поправил код ДЗ

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-let minutes = 46;
+let minutes = 17;
 
 if (!(minutes >=0 && minutes <=59)) 
 {
@@ -8,15 +8,15 @@ if (!(minutes >=0 && minutes <=59))
 {
     console.log(`${minutes} минут - это первая половина часа`);
 }
-  else if (minutes >= 15 && minutes >= 30) 
+  else if (minutes >= 15 && minutes <= 30) 
 {
     console.log(`${minutes} минут - это вторая четверть часа`);
 }
-else if (minutes >= 30 && minutes >= 45) 
+else if (minutes >= 31 && minutes <= 45) 
 {
     console.log(`${minutes} минут - это третья четверть часа`);
 }
-else if (minutes >= 46 && minutes >= 59) 
+else if (minutes >= 46 && minutes <= 59) 
 {
     console.log(`${minutes} минут - это четвертая четверть часа`);
 }
