e9682c2bb562fee94e7327d34c85c267bade9771
Ivan <97340485+IvanBakharev@users.noreply.github.com>
2024-05-18 20:12:45 +0300

    Home Work 2 complete

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 
-let minutes = 43;
+let minutes = 25;
 
 if (!(minutes >=0 && minutes <=59)) 
 {
@@ -22,6 +22,39 @@ else if (minutes >= 46 && minutes <= 59)
     console.log(`${minutes} минут - это четвертая четверть часа`);
 }
 
-
 let result = "";
-let month = 2;
+let month = 5;
+
+if(!(Number.isInteger(month))) {
+    console.log("Введите целое значение от 1 до 12!");
+}
+
+else if (month > 0 && month <= 12) {
+    if (month < 3 || month === 12) {
+        result = "Зима";
+    }
+    else if (month >= 3 && month < 6) {
+        result = "Весна";
+    }
+    else if (month >= 6 && month < 9) {
+        result = "Лето";
+    }
+    else if (month >= 9 && month < 12) {
+        result = "Осень";
+    }
+}
+else {
+    result = "Введите целое значение от 1 до 12! <br>";
+}
+console.log(result);
+
+let num = 5;
+result = "";
+
+if (num % 2 === 0) {
+    result = "Возвожу в квадрат:" + Math.pow(num, 2);
+}
+else {
+    result = "Возвожу в третью степень:" + Math.pow(num, 3);
+}
+console.log(result);
